13cec8af682c6a07beb86aa02630a25a074a23bd
SANJAY PATTED <sanjaypattedbng@gmail.com>
2025-05-01 12:50:37 +0530

    intermediate

--- beginnerpgms.js
+++ beginnerpgms.js
@@ -122,3 +122,106 @@ async function delay_for_two_sec(){
 }
 
 delay_for_two_sec();
+
+
+//practicing the promise
+
+
+function simulateDownload(){
+    return new Promise((resolve,reject)=>{
+        console.log('starting download......');
+        setTimeout(()=>{
+            const success=true;
+            if(success){
+                resolve('download complete !!!!');
+            }else{
+                reject('xxxxxx download failed xxxxxx');
+            }
+        },2000);
+    });
+}
+
+
+simulateDownload()
+    .then(result=>{
+        console.log('success :',result);
+    
+
+    })
+    .catch(error=>{
+        console.log('error :',error);
+    });
+
+
+
+//same above example using async and await but instead of then and catch
+
+async function startDownload(){
+    try{
+        const result=await simulateDownload();
+        console.log('success',result);
+
+    }catch(error){
+        console.log('error',error)
+
+    }
+}
+startDownload();
+
+
+
+//fetching some user data given in resolve:
+function loginUser(username){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            console.log(`logging in as ==> ${username} `);
+            console.log('<<<<this is waiting time>>>')
+            resolve({ username: username });
+        }, 1000);
+    });
+}
+
+
+function getUserData(user){
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+        console.log('fetched user details ');
+        resolve({user:user,
+                age:25,
+                email:'sanjay@gmail.com'
+                });    
+        }, 1500);
+    })
+}
+
+async function start(){
+    try{
+        const user=await loginUser('sanjay');
+        const profile=await getUserData(user);
+        console.log('login was successfull !!!!');
+        console.log('profile :',profile);
+    }catch(error){
+        console.log('something went wrong:',error);
+
+    }
+}
+start();
+//this is how workflow happens in real apps and then update with the ui 
+
+
+//pausing the execution using await
+function getMsg(){
+    console.log('this is getMsg function mesage');
+    console.log('next here below it is printing the actual msg that is from greet async func')
+    return new Promise(resolve => resolve("Hello"));
+    
+}
+async function greet() {
+    const msg=await getMsg();
+    console.log(msg);
+}
+greet();
+
+// here we can also add setTimout to just delay the printing or fetching the message
+
+
